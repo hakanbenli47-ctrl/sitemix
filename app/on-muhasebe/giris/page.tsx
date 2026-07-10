7bc98e2962683ab3de66dc2936b5bc1c235839df
hackhakan13255 <hakanbenli47@gmail.com>
2026-07-10 10:36:23 +0300

    songnceleme

--- app/on-muhasebe/giris/page.tsx
+++ app/on-muhasebe/giris/page.tsx
@@ -371,7 +371,7 @@ export default function OnMuhasebeGirisPage() {
                     name="email"
                     value={email}
                     onChange={(event) => setEmail(event.target.value)}
-                    placeholder="ornek@mail.com veya hakan benli"
+                    placeholder="@gmail.com "
                     autoComplete="username"
                     className="h-14 w-full rounded-2xl bg-slate-100 px-4 text-sm font-bold text-[#0b1025] outline-none transition placeholder:text-slate-400 focus:bg-white focus:ring-4 focus:ring-indigo-100"
                   />
